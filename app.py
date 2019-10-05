f584c24240f6c9aeae50aa5542a67dce3a1d13e9
sutthisak <sliwsakul@hotmail.com>
2019-10-05 17:01:29 +0700

    add latelimit header

--- app.py
+++ app.py
@@ -7,7 +7,8 @@ app = Flask(__name__)
 limiter = Limiter(
     app,
     key_func=get_remote_address,
-    default_limits=['200 per day', '50 per hour']
+    default_limits=['200 per day', '50 per hour'],
+    headers_enabled=True
 )
 
 @app.errorhandler(429)
