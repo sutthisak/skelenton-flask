6cfb0c1bb3ee8be70285b5e4e40f1e42ecc612eb
sutthisak <sliwsakul@hotmail.com>
2019-10-05 16:04:46 +0700

    update

---
+++ app.py
@@ -0,0 +1,28 @@
+from flask import (Flask, request, jsonify)
+
+app = Flask(__name__)
+
+def get_header_data(header_name):
+    if header_name in request.headers:
+        return request.headers[header_name]
+    else:
+        return None
+
+def check_authorization(api_token):
+    if api_token == '1234567890':   # Todo : Check api_token implementation
+        return True
+    else:
+        return False
+
+@app.route('/', methods=['GET', 'POST'])
+def default():
+    if(check_authorization(get_header_data('X-Api-Token')) == False):
+        return jsonify({'message': 'ERROR: Unauthorized'}), 401
+
+    if request.method == 'POST':        
+        return jsonify({'message': 'OK POST: Authorized'}), 200
+    else:
+        return jsonify({'message': 'OK GET: Authorized'}), 200
+
+if __name__ == '__main__':
+   app.run(host="0.0.0.0", use_reloader=True, debug=True)
